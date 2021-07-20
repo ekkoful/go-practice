43c581b805d136147bd692750f1c8872162af4b8
ekkoful <1321735387@qq.com>
2021-07-20 20:57:38 +0800

    add recovery

--- 7days-golang/ekkogee/ekkogee/ekkogee.go
+++ 7days-golang/ekkogee/ekkogee/ekkogee.go
@@ -2,6 +2,9 @@ package ekkogee
 
 import (
 	"net/http"
+	"path"
+	"strings"
+	"text/template"
 )
 
 // HandlerFunc defines the request handler by ekkogee
@@ -19,8 +22,10 @@ type RouterGroup struct {
 type Engine struct {
 	//router map[string]HandlerFunc
 	*RouterGroup
-	router *router
-	groups []*RouterGroup
+	router        *router
+	groups        []*RouterGroup
+	htmlTemplates *template.Template // for html render
+	funcMap       template.FuncMap   // for html render
 }
 
 func New() *Engine {
@@ -35,7 +40,7 @@ func (group *RouterGroup) Group(prefix string) *RouterGroup {
 	newGroup := &RouterGroup{
 		prefix: group.prefix + prefix,
 		parent: group,
-		engine: engine
+		engine: engine,
 	}
 	engine.groups = append(engine.groups, newGroup)
 	return newGroup
@@ -43,13 +48,34 @@ func (group *RouterGroup) Group(prefix string) *RouterGroup {
 
 func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
 	pattern := group.prefix + comp
-	log.Printf("Route %4s - %s", method, pattern)
+	//log.Printf("Route %4s - %s", method, pattern)
 	group.engine.router.addRoute(method, pattern, handler)
 }
 
 /* func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
 	engine.router.addRoute(method, pattern, handler)
 } */
+
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		// Check if file exists and/or if we have permission to access it
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(c.Writer, c.Req)
+	}
+}
+
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	group.GET(urlPattern, handler)
+}
 
 func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
 	group.addRoute("GET", pattern, handler)
@@ -59,8 +85,29 @@ func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
 	group.addRoute("POST", pattern, handler)
 }
 
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
+}
+
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+
 	c := newContext(w, req)
+	c.handlers = middlewares
+	c.engine = engine
 	engine.router.handle(c)
 }
 
--- 7days-golang/ekkogee/main.go
+++ 7days-golang/ekkogee/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"ekkogee/ekkogee"
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -10,29 +12,42 @@ func main() {
 	r := ekkogee.New()
 
 	r.GET("/", func(c *ekkogee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+
+	r.GET("/panic", func(c *ekkogee.Context) {
+		names := []string{"ekkoful"}
+		c.String(http.StatusOK, names[100])
+	})
+
+	v1 := r.Group("/v1")
+	v1.GET("/", func(c *ekkogee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello ekkoGee</h1>")
 	})
 
-	r.GET("/hello", func(c *ekkogee.Context) {
+	v1.GET("/hello", func(c *ekkogee.Context) {
 		// expect /hello?name=geektutu
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.GET("/hello/:name", func(c *ekkogee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *ekkogee.Context) {
-		c.JSON(http.StatusOK, ekkogee.H{"filepath": c.Param("filepath")})
-	})
-
-	r.POST("/login", func(c *ekkogee.Context) {
-		c.JSON(http.StatusOK, ekkogee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
+	{
+		v2.GET("/hello/:name", func(c *ekkogee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-	})
-
+	}
 	r.Run(":9999")
+}
+
+func onlyForV2() ekkogee.HandlerFunc {
+	return func(c *ekkogee.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
 }
