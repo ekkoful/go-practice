9cac70c13d327ee60c57b5a2087480733df70286
ekkoful <1321735387@qq.com>
2021-07-24 00:23:10 +0800

    add arraylist

--- DataStructure/arrayList/arrayList.go
+++ DataStructure/arrayList/arrayList.go
@@ -1,12 +1,27 @@
 package arraylist
 
 type ArrayList struct {
-	Element []interface{}
-	Size    int
+	element []interface{}
+	size    int
 }
 
 func New(values ...interface{}) *ArrayList {
 	list := &ArrayList{}
-	list.Element = make([]interface{}, 10)
+	//list.Element = make([]interface{}, 10)
+	if len(values) > 0 {
+		list.Add(values)
+	}
+	return list
+}
+
+func (list *ArrayList) Add(values ...interface{}) {
+	list.growBy(len(values))
+	for _, value := range values {
+		list.element[list.size] = value
+		list.size++
+	}
+}
+
+func (list *ArrayList) growBy(s int) {
 
 }
