3b3b986d3f20c8f552a38a42a3763c5a03735ce9
ekkoful <1321735387@qq.com>
2021-07-27 22:51:25 +0800

    add arraylist test

--- DataStructure/arrayList/arrayList_test.go
+++ DataStructure/arrayList/arrayList_test.go
@@ -1,1 +1,17 @@
 package arraylist
+
+import (
+	"testing"
+)
+
+func TestListNew(t *testing.T) {
+	list1 := New()
+	if actualValue := list1.IsEmpty(); actualValue != true {
+		t.Errorf("Got %v expected %v", actualValue, true)
+	}
+
+	list2 := New()
+	if actualValue := list2.Size(); actualValue != 2 {
+		t.Errorf("Got %v expected %v", actualValue, 2)
+	}
+}
