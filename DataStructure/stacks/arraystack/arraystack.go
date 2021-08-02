c305f158647b2457905712b73e4be56e3b43b793
ekkoful <1321735387@qq.com>
2021-08-02 18:26:54 +0800

    add arraystack

--- DataStructure/stacks/arraystack/arraystack.go
+++ DataStructure/stacks/arraystack/arraystack.go
@@ -1,1 +1,14 @@
 package arraystack
+
+import (
+	"DataStructure/lists/arraylist"
+)
+
+type Stack struct {
+	stack *arraylist.ArrayList
+}
+
+func New() *Stack {
+	stack := &Stack{}
+	return stack
+}
