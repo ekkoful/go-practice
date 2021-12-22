6aeb1bcbeb4d20f385f852953a43a0d15a2df1a6
ekkoful <1321735387@qq.com>
2021-12-22 23:05:01 +0800

    feat(arraystack.go): add the array stack methods

--- DataStructure/stacks/arraystack/arraystack.go
+++ DataStructure/stacks/arraystack/arraystack.go
@@ -5,10 +5,38 @@ import (
 )
 
 type Stack struct {
-	stack *arraylist.ArrayList
+	list *arraylist.ArrayList
 }
 
 func New() *Stack {
-	stack := &Stack{}
+	stack := &Stack{
+		list: arraylist.New(),
+	}
 	return stack
+}
+
+func (stack *Stack) Push(value ...interface{}) {
+	stack.list.Add(value)
+}
+
+func (stack *Stack) Pop() (value interface{}, ok bool) {
+	value, ok = stack.list.Get(stack.list.Size() - 1)
+	stack.list.Remove(stack.list.Size() - 1)
+	return
+}
+
+func (stack *Stack) Empty() bool {
+	return stack.list.IsEmpty()
+}
+
+func (stack *Stack) Size() int {
+	return stack.list.Size()
+}
+
+func (stack *Stack) Clear() {
+	stack.list.Clear()
+}
+
+func (stack *Stack) Peek() (value interface{}, ok bool) {
+	return stack.list.Get(stack.list.Size() - 1)
 }
